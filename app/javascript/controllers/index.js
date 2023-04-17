4c82311daf13bf84e8484dc40b3ce5ee72289956
Фанис Хабибуллин <userfan@MacBook-Air-Fanis.local>
2023-04-17 21:20:02 +0300

    FixMergeAddMission

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,9 +7,6 @@ import { application } from "./application"
 import FlatpickrController from "./flatpickr_controller"
 application.register("flatpickr", FlatpickrController)
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import ModalController from "./modal_controller"
 application.register("modal", ModalController)
 
