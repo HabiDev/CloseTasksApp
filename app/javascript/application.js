1d8e7f0f13a9a8b9232282306493fbc7cbfe78d4
Фанис Хабибуллин <userfan@MacBook-Air-Fanis.local>
2025-01-26 20:27:44 +0300

    Fix app_files load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,10 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "./direct_uploads"
 import * as bootstrap from "bootstrap"
+import * as ActiveStorage from "@rails/activestorage"
+ActiveStorage.start()
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
